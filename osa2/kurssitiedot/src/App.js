e3bfe73733be7b80d53f77592ca48695cd6d15bb
Taavi Kokko <taavi.kokko@gmail.com>
2021-03-22 18:24:25 +0200

    Tehty 2.1-1.5

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import {Course} from './Course'
+
+const App = () => {
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
+
+    courses.forEach(course =>
+        course.total = course.parts.reduce((acc, curr) => acc + curr.exercises, 0)
+    )
+
+
+    return (
+        courses.map(course =>
+            <div>
+                <Course course={course} />
+            </div>
+        )
+    )
+}
+
+
+
+export default App
---
+++ osa2/kurssitiedot/src/Course.js
@@ -0,0 +1,43 @@
+const Header = props => {
+    return (
+        <h1>{props.name}</h1>
+    )
+}
+
+const Content = props => {
+    return (
+        <ul>
+            {
+                props.parts.map(part =>
+                    <Part part={part.name} exercises={part.exercises} key={part.name} />
+                )
+            }
+        </ul>
+    )
+}
+
+const Part = props => {
+    return (
+        <li>
+            {props.part} {props.exercises}
+        </li>
+    )
+}
+
+const Total = props => {
+    return (
+        <h3>
+            Total of {props.total} exercises
+        </h3>
+    )
+}
+
+export const Course = props => {
+    return (
+        <div>
+        <Header name={props.course.name} />
+        <Content parts={props.course.parts} />
+        <Total total={props.course.total} />
+    </div>
+)
+}
