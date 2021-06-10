9139e10cf078fa8fc02726f4373634884a4d0d48
Taavi Kokko <taavi.kokko@gmail.com>
2021-06-10 12:39:00 +0300

    6.1, 6.2 tehty

---
+++ osa6/unicafe-redux/src/reducer.js
@@ -0,0 +1,36 @@
+const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+    console.log(action)
+    switch (action.type) {
+        case 'GOOD':
+            const newStateGood= {
+                ...state,
+                good: state.good + 1
+            }
+            return newStateGood
+        case 'OK':
+            const newStateOk= {
+                ...state,
+                ok: state.ok + 1
+            }
+            return newStateOk
+        case 'BAD':
+            const newStateBad= {
+                ...state,
+                bad: state.bad + 1
+            }
+            return newStateBad
+        case 'ZERO':
+            return initialState
+        default:
+            return state
+    }
+
+}
+
+export default counterReducer
