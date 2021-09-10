54f3043880a4e9ef847001e7b9d10a8d1dd6ba92
Taavi Kokko <taavi.kokko@gmail.com>
2021-09-10 12:10:49 +0300

    7.17 aloitettu

--- osa7/bloglist-extended/backend/models/comment.js
+++ osa7/bloglist-extended/backend/models/comment.js
@@ -1,22 +1,20 @@
 const mongoose = require('mongoose')
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-    user: {
+const commentSchema = mongoose.Schema({
+    comment: String,
+    blog: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'Blog'
     }
+
 })
 
-blogSchema.set('toJSON', {
+commentSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
-    }
+    },
 })
 
-module.exports = mongoose.model('Blog', blogSchema)
+module.exports = mongoose.model('Comment', commentSchema)
